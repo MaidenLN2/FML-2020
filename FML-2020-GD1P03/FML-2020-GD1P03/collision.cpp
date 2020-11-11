3cfb4442c78f91f05ff2039bfd9b2f16a4977a17
MaidenLN2 <65572330+MaidenLN2@users.noreply.github.com>
2020-11-11 17:08:44 +1300

    E R R O R S and new codes

--- FML-2020-GD1P03/FML-2020-GD1P03/collision.cpp
+++ FML-2020-GD1P03/FML-2020-GD1P03/collision.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "collision.h"
+#include "Player.h"
 
 // Player goes SLAM with enemy
 //bool collision::collidesWith(Player&, Enemy&)
@@ -101,3 +102,32 @@
 //{
 //    return false;
 //}
+
+//for (unsigned int i = 0; i <= solidObjects.size() - 1; i++)
+//{
+//    if (solidObjects[i].getGlobalBounds().intersects(Player.getGlobalBounds()))
+//    {
+//        if (Player.getPosition().x - solidObjects[i].getPosition().x < Player.getPosition().y - solidObjects[i].getPosition().y)
+//        {
+//            Player.setPosition(solidObjects[i].getPosition().x - Player.getOrigin().x, SuperMario.getPosition().y);
+//            IsTouching = false;
+//        }
+//        else if (Player.getPosition().y - solidObjects[i].getPosition().y < Player.getPosition().x - solidObjects[i].getPosition().x)
+//        {
+//            Player.setPosition(Player.getPosition().x, solidObjects[i].getPosition().y - Player.getOrigin().y);
+//            IsTouching = true;
+//        }
+//        else if (Player.getPosition().x - solidObjects[i].getTextureRect().width < Player.getPosition().y - solidObjects[i].getPosition().y)
+//        {
+//            Player.setPosition(solidObjects[i].getTextureRect().width + Player.getOrigin().x, Player.getPosition().y);
+//            IsTouching = false;
+//        }
+//    }
+//    else
+//    {
+//        IsTouching = false;
+//    }
+//}
+
+Tile platform(texture_sheet, texture_rect, x, y, scale);
+platform.getGlobalBounds().intersects(rect.GlobalBounds))
--- FML-2020-GD1P03/FML-2020-GD1P03/collision.h
+++ FML-2020-GD1P03/FML-2020-GD1P03/collision.h
@@ -5,6 +5,7 @@
 #include "Projectile.h"
 #include "Tile.h"
 #include "stdafx.h"
+#include "Player.h"
 
 // Creating class collision between objects I shoved up my code
 
@@ -16,6 +17,43 @@ public:
     static bool collidesWith(Projectile&, Player&);
     static bool collidesWith(Tile& obj1, Player& obj2);
 
+   
+
 private:
 
 };
+bool sf::Player::collision(sf::Sprite Tile)
+{
+    this->setBounds();
+    Tile.getGlobalBounds();
+
+    if (top > Tile.bottom || bottom < sprite2.top || left > sprite2.right || right < sprite2.left) 
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void sf::Player::colMove(sf::Vector2f& movement, sf::Tile sprite2) 
+{
+    if (!this->collision(Tile)) 
+    {
+        this->move(movement);
+    }
+}
+
+void sf::Sprite::colMove(float x, float y, sf::Sprite sprite2) 
+{
+    if (!this->collision(sprite2)) 
+    {
+        this->move(x, y);
+    }
+}
+
+void sf::Player::setBounds() {
+    top = this->getPosition().y;
+    bottom = this->getPosition().y + this->getTexture()->getSize().y;
+    left = this->getPosition().x;
+    right = this->getPosition().x + this->getTexture()->getSize().y;
+}
